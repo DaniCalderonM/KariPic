553ea30988859ebfd46c8fb21223d52fcf03a587
danicalderonm <mesiasd.calderon@gmail.com>
2023-08-10 18:21:49 -0400

    segundo commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'homes/index'
   resources :kpposts
   devise_for :kpusers
   resources :kpcomments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :kpusers, only: [:show, :edit, :update]
   # Defines the root path route ("/")
+  #  root "homes#index"
    root "kpposts#index"
 end
